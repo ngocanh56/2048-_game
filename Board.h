e8569a3002a958042c78d288763c25e03d50a763
ngocanh56 <ngocanhthlfb@gmail.com>
2023-04-11 10:50:34 +0700

    speed animation

--- Board.h
+++ Board.h
@@ -124,7 +124,7 @@ void Board::modifyBoard(Direction check)
         }
         if(quit==true && num==1) return;
         //do not create new cells if not moving
-        SDL_Delay(30);
+        SDL_Delay(5);
     }
 
     music=Mix_LoadMUS("Sound/merge1.wav");
